76f4bd1cc9cc7a0a38484bb772a767e91c836e16
Princi Gupta <princygupta037@gmail.com>
2019-12-10 23:34:02 +0530

    blog

--- princiltd/settings.py
+++ princiltd/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5(upnr@d6^o$na+grrz(jn#b37pcw^gbb-4^bow-q9^4a2a85a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
